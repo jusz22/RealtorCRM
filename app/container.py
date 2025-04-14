26102787d570a36a943253f284fac163e04f8701
jusz22 <jasiuswag@gmail.com>
2025-04-14 13:49:56 +0200

    clean up

--- app/container.py
+++ app/container.py
@@ -5,52 +5,32 @@ from app.application.interfaces.services.client_service import ClientService
 from app.application.interfaces.services.email_service import EmailService
 from app.application.interfaces.services.graph_service import GraphService
 from app.application.interfaces.services.listing_service import ListingService
+from app.application.interfaces.services.user_service import UserService
+from app.infrastructure.db import async_session
 from app.infrastructure.repositories.client_repository import ClientRepository
 from app.infrastructure.repositories.listing_repository import ListingRepository
 from app.infrastructure.repositories.user_repository import UserRepository
-from app.application.interfaces.services.user_service import UserService
-from app.infrastructure.db import async_session
+
 
 class Container(DeclarativeContainer):
     db = Singleton(async_session)
 
-    client_repository = Singleton(
-        ClientRepository,
-        session = db
-    )
-    
-    client_service = Singleton(
-        ClientService,
-        repository = client_repository
-    )
+    client_repository = Singleton(ClientRepository, session=db)
 
-    user_repository = Factory(
-        UserRepository,
-        session = db
-    )
+    client_service = Singleton(ClientService, repository=client_repository)
 
-    user_service = Factory(
-        UserService,
-        repository = user_repository
-    )
+    user_repository = Factory(UserRepository, session=db)
 
-    listing_repository = Factory(
-        ListingRepository,
-        session = db
-    )
-    
-    listing_service = Factory(
-        ListingService,
-        repository = listing_repository
-    )
-    
-    graph_service = Singleton(
-        GraphService,
-        repository = listing_repository
-    )
+    user_service = Factory(UserService, repository=user_repository)
+
+    listing_repository = Factory(ListingRepository, session=db)
+
+    listing_service = Factory(ListingService, repository=listing_repository)
+
+    graph_service = Singleton(GraphService, repository=listing_repository)
 
     email_service = Singleton(
         EmailService,
-        repository = listing_repository,
+        repository=listing_repository,
         graph_service=graph_service,
     )
--- app/presentation/schemas/token_schema.py
+++ app/presentation/schemas/token_schema.py
@@ -1,12 +1,16 @@
 from datetime import datetime
+
 from pydantic import BaseModel
+
 
 class Token(BaseModel):
     access_token: str
     token_type: str
 
+
 class TokenData(BaseModel):
     username: str
+
 
 class TokenPayload(BaseModel):
     sub: str = None
