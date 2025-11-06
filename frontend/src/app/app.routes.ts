f4368b7f9a8583dc14428022a7a10d4106777d52
jusz22 <jasiuswag@gmail.com>
2025-11-06 02:10:52 +0100

    add: home component and some routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,23 +1,38 @@
 import { Routes } from '@angular/router';
 import { Login } from './shared/components/login/login';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { authGuard } from './core/guards/auth-guard';
 import { ClientsComponent } from './shared/components/clients/clients';
-import { App } from './app';
+import { ListingsComponent } from './core/components/listings/listings.component';
+import { HomeComponent } from './core/components/home/home.component';
+import { ListingComponent } from './core/components/listings/listing/listing.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: App,
+    path: 'listings/:id',
+    component: ListingComponent,
     canActivate: [authGuard],
+  },
+  {
+    path: 'listings',
+    component: ListingsComponent,
+    canActivate: [authGuard],
+    title: 'Listings',
   },
   {
     path: 'clients',
     component: ClientsComponent,
     canActivate: [authGuard],
+    title: 'Clients',
   },
   {
     path: 'login',
     component: Login,
+    title: 'Sign in',
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [authGuard],
+    title: 'RealtorCRM',
   },
 ];
