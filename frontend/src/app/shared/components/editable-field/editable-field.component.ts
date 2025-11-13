c9794b649c37fc347d49407572c6de4d60703b2e
jusz22 <jasiuswag@gmail.com>
2025-11-13 01:47:23 +0100

    refactor: replace PrimeNG comoponets with custom ones

--- frontend/src/app/shared/components/editable-field/editable-field.component.ts
+++ frontend/src/app/shared/components/editable-field/editable-field.component.ts
@@ -2,22 +2,25 @@ import { Component, model, output, signal } from '@angular/core';
 import { InplaceModule } from 'primeng/inplace';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'sha-editable-field',
   templateUrl: 'editable-field.component.html',
-  imports: [InplaceModule, InputTextModule, ButtonModule],
+  imports: [InplaceModule, InputTextModule, ButtonModule, FormsModule],
 })
 export class EditableFieldComponent {
   readonly data = model.required<{ key: string; value: string | number | Date }>();
   protected readonly editMode = signal(false);
   readonly updateField = output<{}>();
 
-  onClickText() {
+  onClickInput() {
     this.editMode.set(true);
+    console.log('dasdsadas');
   }
   onClose() {
     this.editMode.set(false);
+    console.log('close');
   }
   onSave(inputValue: ReturnType<typeof this.data>) {
     if (inputValue !== this.data()) {
