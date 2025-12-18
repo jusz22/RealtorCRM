a7dfa62b4863037acb4ce742f666ce4b1c983154
jusz22 <jasiuswag@gmail.com>
2025-12-18 08:12:41 +0100

    fix: auth checking

--- frontend/src/app/core/interceptors/error.interceptor.ts
+++ frontend/src/app/core/interceptors/error.interceptor.ts
@@ -2,14 +2,17 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, Observable, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
   private readonly router = inject(Router);
+  private readonly authService = inject(AuthService);
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((err) => {
         if (err.status === 401) {
+          this.authService.deleteToken();
           this.router.navigateByUrl('/login');
         }
         return throwError(() => err.error);
